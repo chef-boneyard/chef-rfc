0dc2303fcbeeee13e66dcf79aa99a41cdc98321f
Thom May <thom@chef.io>
2016-01-11 09:59:17 +0000

    Generate GitHub pages for RFC

--- Rakefile
+++ Rakefile
@@ -2,4 +2,5 @@ require 'rubygems'
 require 'rubygems/package_task'
 require 'rdoc/task'
 require_relative 'tasks/advocates'
+require_relative 'tasks/pages'
 
---
+++ tasks/pages.rb
@@ -0,0 +1,103 @@
+# Generate https://chef.github.io/chef-rfc/ , using metadata from
+# individual RFCs to build an index.
+
+require 'metadown'
+require 'pygments'
+require 'erubis'
+
+class HTMLwithPygments < Metadown::Renderer
+  def block_code(code, language)
+    Pygments.highlight(code, lexer: language)
+  rescue Exception
+    Pygments.highlight(code, lexer: 'text')
+  end
+end
+
+POSTAMBLE=<<-EOF.freeze
+    </div>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.3/fastclick.min.js"></script>
+    <script src="https://style.chef.io/dist/1.0.1/javascripts/chef.js"></script>
+    <script>
+      $(document).chef({
+        assets: {
+          images: 'https://style.chef.io/assets/images'
+        }
+      });
+    </script>
+  </body>
+</html>
+EOF
+
+PREAMBLE=<<-EOF.freeze
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable = no">
+    <title>Chef RFCs</title>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href="/images/favicon.ico" rel="shortcut icon">
+    <script src="https://style.chef.io/dist/0.1.8/javascripts/vendor/modernizr.js"></script>
+    <link rel="stylesheet" href="https://style.chef.io/dist/1.0.1/stylesheets/chef.css">
+  </head>
+  <body>
+    <div class="container">
+      <header class="clearfix">
+        <div class="left">
+          <a href="http://chef.github.io"><h1>Chef RFCs</h1></a>
+        </div>
+      </header>
+EOF
+
+INDEX=<<-EOF.freeze
+<section class="rfcs">
+<table>
+<tr>
+  <th>Number</th>
+  <th>Title</th>
+  <th>Author</th>
+  <th>Type</th>
+  <th>Status</th>
+  <th>Tracking</th>
+</tr>
+<% for item in metadata -%>
+<tr>
+  <td><a href="<%= item["Filename"] %>"><%= item["RFC"] %></a></td>
+  <td><a href="<%= item["Filename"] %>"><%= item["Title"] %></a></td>
+  <td><%= item["Author"] %></td>
+  <td><%= item["Type"] %></td>
+  <td><%= item["Status"] %></td>
+  <td><%= item["Tracking"] %></td>
+</tr>
+<% end -%>
+</table>
+</section>
+EOF
+
+namespace :pages do
+  desc "Generate GitHub Pages for RFCs"
+  task :generate do
+    sh("git worktree add gh-pages gh-pages") unless File.directory?("gh-pages")
+    metadata = []
+    renderer = Redcarpet::Markdown.new(HTMLwithPygments, :fenced_code_blocks => true)
+    Dir[File.join(File.dirname(__FILE__), "..", "rfc*.md")].each do |rfc|
+      name = File.basename(rfc, ".md")
+      target = name + ".html"
+      output = Metadown.render(File.read(rfc), renderer)
+      output.metadata ||= {}
+      output.metadata["Filename"] = target
+      number, title = name.match(/rfc(\d*)-(.+)/)[1,2]
+      output.metadata["Title"] ||= title.split("-").map(&:capitalize).join(" ")
+      output.metadata["RFC"] ||= number
+      metadata << output.metadata
+      File.open(File.join("gh-pages", target), "w") {|fh| fh.write(PREAMBLE + output.output + POSTAMBLE) }
+    end
+    eruby = Erubis::EscapedEruby.new(INDEX)
+    File.open(File.join("gh-pages", "index.html"), "w") {|fh| fh.write(PREAMBLE + eruby.result(binding()) + POSTAMBLE) }
+  end
+  puts "Now enter the gh-pages directory and git diff etc"
+  puts "The site will get updated when you commit and push the gh-pages branch with your changes"
+end
